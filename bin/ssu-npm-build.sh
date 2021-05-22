76a181be206e95b13a4e1d4f417f0bebad61c769
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-05-22 17:27:21 +1200

    WIP

---
+++ bin/ssu-npm-build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORKING_DIR=$(pwd)
+# : ${stanConfig:=$SCRIPT_DIR/../sunnysideup/easy-coding-standards/phpstan.neon}
+
+if [ "$1" != "" ]; then
+    dir=$1;
+else
+    dir='themes/client'
+fi
+
+help_and_exit() {
+    echo " ---------------------------------";
+    echo "   Available settings:";
+    echo " ---------------------------------";
+    echo "   none"
+    echo " ---------------------------------";
+    echo "   Example usage:"
+    echo " ---------------------------------";
+    echo "   ssu-npm-build themes/client";
+    echo " ---------------------------------";
+    echo " "
+    echo " "
+    exit;
+}
+
+while (( $# )); do
+  case $1 in
+    -*)                printf 'Unknown option: %q\n\n' "$1";
+                       help_and_exit 1 ;;
+    *)                 dir=$1;;
+  esac
+  shift
+done
+
+echo " "
+echo " "
+echo " ---------------------------------";
+echo "   Running NPM build";
+echo " ---------------------------------";
+echo "   Directory of script:              $SCRIPT_DIR";
+echo "   Directory to install:             $WORKING_DIR/$dir";
+echo " ---------------------------------";
+echo " "
+echo " "
+
+cd themes/sswebpack_engine_only/
+npm run build --theme_dir=$dir
