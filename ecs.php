a41933de8e3f74db145f15454f4577c53aa4bba6
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-06-12 21:03:18 +1200

    WIP

--- ecs.php
+++ ecs.php
@@ -6,6 +6,7 @@ use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\Basic\Psr4Fixer;
 use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
 use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
@@ -48,35 +49,40 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::LINE_ENDING, "\n");
 
     $parameters->set(
-        Option::SETS,
-        [
-            // run and fix, one by one
-            // SetList::SPACES,
-            // SetList::ARRAY,
-            // SetList::STRICT,
-            // SetList::SYMFONY,
-            SetList::DOCBLOCK,
-            SetList::NAMESPACES,
-            // SetList::SYMPLIFY,
-            SetList::COMMON,
-            SetList::COMMENTS,
-            // SetList::CONTROL_STRUCTURES,
-            SetList::CLEAN_CODE,
-            // SetList::PSR_1,
-            SetList::PHP_CS_FIXER,
-            SetList::PSR_12,
-            // SetList::PHP_CS_FIXER_RISKY,
-            // SetList::PHPUNIT,
-        ]
-    );
-
-    $parameters->set(
         Option::SKIP,
         [
             SelfAccessorFixer::class,
             ArrayOpenerAndCloserNewlineFixer::class,
             ArrayListItemNewlineFixer::class,
             OrderedClassElementsFixer::class,
+            DeclareStrictTypesFixer::class,
         ]
     );
+
+
+    $containerConfigurator->import(SetList::DOCBLOCK);
+    $containerConfigurator->import(SetList::NAMESPACES);
+    $containerConfigurator->import(SetList::COMMON);
+    $containerConfigurator->import(SetList::COMMENTS);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::PHP_CS_FIXER);
+    $containerConfigurator->import(SetList::PSR_12);
+
+    // // run and fix, one by one
+    // // SetList::SPACES,
+    // // SetList::ARRAY,
+    // // SetList::STRICT,
+    // // SetList::SYMFONY,
+    // SetList::DOCBLOCK,
+    // SetList::NAMESPACES,
+    // // SetList::SYMPLIFY,
+    // SetList::COMMON,
+    // SetList::COMMENTS,
+    // // SetList::CONTROL_STRUCTURES,
+    // SetList::CLEAN_CODE,
+    // // SetList::PSR_1,
+    // SetList::PHP_CS_FIXER,
+    // SetList::PSR_12,
+    // // SetList::PHP_CS_FIXER_RISKY,
+    // // SetList::PHPUNIT,
 };
