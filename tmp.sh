b33f848dcdc5ef1f9aacf174bdf7b120e23e72ad
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-01-05 15:17:05 +1300

    PATCH: fixing submission to packagist

--- tmp.sh
+++ tmp.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+response=$(curl -L -X POST "https://packagist.org/api/create-package?username=sunnysideup&apiToken=3bfle73lwrwgwsckgcsw" -H "Content-Type: application/json" -d "{\"repository\":{\"url\":\"https://github.com/sunnysideup/silverstripe-download\"}}")
+echo "$response"
