6bd56b91685d35eb036677d48df146bc2394a171
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-03-04 09:24:00 +1300

    MNT: automated commit

--- ecs.php
+++ ecs.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use PhpCsFixer\Fixer\Basic\Psr4Fixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
 use PhpCsFixer\Fixer\ClassNotation\SelfAccessorFixer;
 use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
@@ -13,43 +14,9 @@ use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]])
-    ;
-
-    $services->set(Psr4Fixer::class);
-
-    $services->set(OrderedImportsFixer::class)
-        ->call(
-            'configure',
-            [[
-                'imports_order' => ['class', 'const', 'function'],
-                'sort_algorithm' => 'alpha', // possible values ['alpha', 'length', 'none']
-            ]]
-        )
-    ;
-
-    // parameters ...
-
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-        ]
-    );
-
-    $parameters->set(Option::LINE_ENDING, "\n");
-
-    $parameters->set(
-        Option::SKIP,
+return ECSConfig::configure()
+    ->withPaths([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->withSkip(
         [
             SelfAccessorFixer::class,
             ArrayOpenerAndCloserNewlineFixer::class,
@@ -57,32 +24,74 @@ return static function (ECSConfig $containerConfigurator): void {
             OrderedClassElementsFixer::class,
             DeclareStrictTypesFixer::class,
         ]
-    );
+    )
+;
+// return static function (ECSConfig $containerConfigurator): void {
+//     $services = $containerConfigurator->services();
+
+//     $services->set(ArraySyntaxFixer::class)
+//         ->call('configure', [[
+//             'syntax' => 'short',
+//         ]])
+//     ;
+
+// $services->set(Psr4Fixer::class);
+
+// $services->set(OrderedImportsFixer::class)
+//     ->call(
+//         'configure',
+//         [[
+//             'imports_order' => ['class', 'const', 'function'],
+//             'sort_algorithm' => 'alpha', // possible values ['alpha', 'length', 'none']
+//         ]]
+//     )
+// ;
+
+// // parameters ...
+
+// $parameters = $containerConfigurator->parameters();
+
+//     $parameters->set(
+//         Option::PATHS,
+//         [
+//             __DIR__ . '/src',
+//             __DIR__ . '/tests',
+//         ]
+//     );
+
+//     $parameters->set(Option::LINE_ENDING, "\n");
+
+//     $parameters->set(
+//         Option::SKIP,
+//         [
+
+//         ]
+//     );
 
 
-    $containerConfigurator->import(SetList::DOCBLOCK);
-    $containerConfigurator->import(SetList::NAMESPACES);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::COMMENTS);
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::PHP_CS_FIXER);
-    $containerConfigurator->import(SetList::PSR_12);
+//     $containerConfigurator->import(SetList::DOCBLOCK);
+//     $containerConfigurator->import(SetList::NAMESPACES);
+//     $containerConfigurator->import(SetList::COMMON);
+//     $containerConfigurator->import(SetList::COMMENTS);
+//     $containerConfigurator->import(SetList::CLEAN_CODE);
+//     $containerConfigurator->import(SetList::PHP_CS_FIXER);
+//     $containerConfigurator->import(SetList::PSR_12);
 
-    // // run and fix, one by one
-    // // SetList::SPACES,
-    // // SetList::ARRAY,
-    // // SetList::STRICT,
-    // // SetList::SYMFONY,
-    // SetList::DOCBLOCK,
-    // SetList::NAMESPACES,
-    // // SetList::SYMPLIFY,
-    // SetList::COMMON,
-    // SetList::COMMENTS,
-    // // SetList::CONTROL_STRUCTURES,
-    // SetList::CLEAN_CODE,
-    // // SetList::PSR_1,
-    // SetList::PHP_CS_FIXER,
-    // SetList::PSR_12,
-    // // SetList::PHP_CS_FIXER_RISKY,
-    // // SetList::PHPUNIT,
-};
+//     // // run and fix, one by one
+//     // // SetList::SPACES,
+//     // // SetList::ARRAY,
+//     // // SetList::STRICT,
+//     // // SetList::SYMFONY,
+//     // SetList::DOCBLOCK,
+//     // SetList::NAMESPACES,
+//     // // SetList::SYMPLIFY,
+//     // SetList::COMMON,
+//     // SetList::COMMENTS,
+//     // // SetList::CONTROL_STRUCTURES,
+//     // SetList::CLEAN_CODE,
+//     // // SetList::PSR_1,
+//     // SetList::PHP_CS_FIXER,
+//     // SetList::PSR_12,
+//     // // SetList::PHP_CS_FIXER_RISKY,
+//     // // SetList::PHPUNIT,
+// };
