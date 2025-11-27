b766b50c17d4c0f7404dab3d1b7d0950d97a0782
Nicolaas <github@sunnysideup.co.nz>
2025-11-28 11:25:31 +1300

    FIX: better silverstripe 6 setup

--- RectorSS6.php
+++ RectorSS6.php
@@ -24,8 +24,7 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
     ->withPhpSets()
     ->withSets([
-        SilverstripeSetList::CODE_STYLE,
-        SilverstripeLevelSetList::UP_TO_SS_6_0
+        SilverstripeSetList::SS_6_0,
     ])
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
