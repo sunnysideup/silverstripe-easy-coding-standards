b798e5ccf25732ad9fffd9c27828f1d92291b65e
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-01-28 07:00:26 +1300

    FIX: set rector back to normal

--- RectorSS6.php
+++ RectorSS6.php
@@ -9,7 +9,6 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        SilverstripeLevelSetList::UP_TO_SS_5_4,
         SilverstripeSetList::SS_6_0
     ]);
 };
