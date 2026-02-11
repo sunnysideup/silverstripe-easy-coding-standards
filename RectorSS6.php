439f67c9ce68d8adef646ed44ac2f552e8f8a2a0
nt@rainbox <nt@sunnysideup.co.nz>
2026-02-11 14:33:54 +1300

    FIX: fixes for easy download

--- RectorSS6.php
+++ RectorSS6.php
@@ -6,9 +6,16 @@ declare(strict_types=1);
 use Netwerkstatt\SilverstripeRector\Set\SilverstripeLevelSetList;
 use Netwerkstatt\SilverstripeRector\Set\SilverstripeSetList;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        SilverstripeSetList::SS_6_0
+        LevelSetList::UP_TO_PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        //silverstripe rector
+        SilverstripeSetList::CODE_STYLE,
+        SilverstripeLevelSetList::UP_TO_SS_6_2
     ]);
 };
